35a800f9f1de60d3e979f223cda983ab047c691f
pedromoraisf <pedro.morais1997@gmail.com>
2020-03-15 04:08:45 -0300

    feature: informe dos usuarios online >> eventos de disconnect

--- src/server.js
+++ src/server.js
@@ -6,33 +6,83 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+const dedupe = require('dedupe');
+
 const PORT = process.env.PORT || 3000;
 
 app.use(
-    '/', (req, res) => {
+  '/', (req, res) => {
 
-        return res.json({
-            message: 'Ta fazendo request pro Chat do Pedrão né...'
-        });
+    return res.json({
+      message: 'Tem nada pra xeretar aqui nao tiw'
+    });
 
-    }
+  }
 );
 
+let users = [];
 let saveMessages = [];
 
 io.on('connection', socket => {
-    console.log(`Socket conectado >> ${socket.id}`);
+  console.log(`Socket conectado >> ${socket.id}`);
+
+  socket.emit('receiveData', saveMessages);
+  socket.emit('receiveUsersList', users);
+
+  socket.on('sendUserInfo', username => {
+
+    // Refresh users
+    users.push({
+      _socketId: socket.id,
+      username
+    });
+    users = dedupe(users);
+
+    // Event emit
+    socket.emit('receiveUsersList', users);
+    socket.broadcast.emit('receiveUsersList', users);
+
+  });
+
+  socket.on('sendMessage', data => {
+
+    // Refresh users
+    users.push({
+      _socketId: socket.id,
+      username: data.username
+    });
+    users = dedupe(users);
+
+
+    // Push messages
+    data._socketId = socket.id
+    data.active
+    saveMessages.push(data);
+
+
+    // Event emit
+    socket.emit('receiveUsersList', users);
+    socket.broadcast.emit('receiveUsersList', users);
 
     socket.emit('receiveData', saveMessages);
+    socket.broadcast.emit('receiveData', saveMessages);
+  });
 
-    socket.on('sendMessage', data => {
-        saveMessages.push(data);
+  socket.on('disconnect', () => {
+    // Treat on users
+    let i = users.indexOf(users.filter(obj => {
+      return obj._socketId === socket.id;
+    })[0]);
 
-        socket.emit('receiveData', saveMessages);
-        socket.broadcast.emit('receiveData', saveMessages);
-    });
+    if (typeof users[i] !== 'undefined')
+      users.splice(i, 1)
+
+    // Event emit
+    socket.emit('receiveUsersList', users);
+    socket.broadcast.emit('receiveUsersList', users);
+  })
 });
 
 server.listen(PORT, () => {
-    console.log(`>> API ON <<`);
+  console.log(`>> API ON <<`);
 });
